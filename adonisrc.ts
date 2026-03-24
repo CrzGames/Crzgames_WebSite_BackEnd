82ebeb4c583381f7d8e6ec4b76a855bc5f2ad6b3
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-24 18:48:14 +0100

    m

--- adonisrc.ts
+++ adonisrc.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     () => import('@adonisjs/lucid/commands'),
     () => import('@adonisjs/session/commands'),
     () => import('@foadonis/crypt/commands'),
-    () => import('@adonisjs/mail/commands')
+    () => import('@adonisjs/mail/commands'),
   ],
 
   /*
