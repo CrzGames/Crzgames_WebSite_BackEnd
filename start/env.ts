603426165dacbb4e957471859ff55c046cbdd17c
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-28 00:40:40 +0100

    m

--- start/env.ts
+++ start/env.ts
@@ -61,4 +61,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   MAIL_FROM_ADDRESS: Env.schema.string(),
   RESEND_API_KEY: Env.schema.string(),
   SESSION_DRIVER: Env.schema.enum(['cookie', 'database', 'memory'] as const),
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
